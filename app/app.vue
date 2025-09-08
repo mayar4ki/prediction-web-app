2c9452369519e8c0d49798f9826846b230f11460
Mayar Deeb <mayar.e.deeb@gmail.com>
2025-09-08 15:03:09 +0400

    init shadcn

--- app/app.vue
+++ app/app.vue
@@ -1,6 +1,19 @@
+<script setup lang="ts">
+import  Button  from '@/components/ui/button/Button.vue';
+
+</script>
+
 <template>
   <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+   
+    <Button variant="outline">
+      koko
+    </Button>
+    <Button >
+      koko
+    </Button>
+    <Button variant="outline">
+      koko
+    </Button>
   </div>
 </template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,20 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  modules: ['@nuxtjs/tailwindcss', 'shadcn-nuxt', '@vueuse/nuxt'],
+  shadcn: {
+    prefix: '',
+    componentDir: './app/components/ui'
+  },
+  typescript: {
+    tsConfig: {
+      compilerOptions: {
+        paths: {
+          '@/*': ['./app/*']
+        }
+      }
+    }
+  }
+
 })
