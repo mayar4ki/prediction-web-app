1091164a378bc07af6a30b1b332d765c8a2c213c
Mayar Deeb <mayar.e.deeb@gmail.com>
2025-09-27 14:38:00 +0400

    fix: translation

--- app/components/home/StatsSection.vue
+++ app/components/home/StatsSection.vue
@@ -34,7 +34,7 @@ const stats = [
       </div>
       <div class="mt-14 grid gap-x-5 gap-y-8 md:grid-cols-2 lg:grid-cols-4">
         <div v-for="stat in stats" :key="stat.id" class="flex flex-col gap-5">
-          <div class="text-6xl font-bold">{{ $t(stat.value) }}</div>
+          <div class="text-6xl font-bold">{{ stat.value }}</div>
           <p>{{ $t(stat.label) }}</p>
         </div>
       </div>
